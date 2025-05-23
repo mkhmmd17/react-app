e8ccfce4cc5a47dc6435a9924d73802758d9d9b5
mkhmmd17 <maxmelhi95@gmail.com>
2025-05-23 19:58:48 -0400

    search query

--- src/App.jsx
+++ src/App.jsx
@@ -20,11 +20,13 @@ const App = () => {
     const [errorMessage, setErrorMessage] = useState('');
 
 
-    const fetchMovies = async () => {
+    const fetchMovies = async (query = '') => {
         setIsLoading(true);
         setErrorMessage('');
         try {
-            const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
+            const endpoint = query
+                ? `${API_BASE_URL}/search/movie?query=${encodeURIComponent(query)}`
+                : `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
             const response = await fetch(endpoint, API_OPTIONS)
             if (!response.ok) {
                 throw new Error('Could not fetch movies');
@@ -48,8 +50,8 @@ const App = () => {
 
 
     useEffect(() => {
-        fetchMovies();
-    }, []);
+        fetchMovies(searchTerm);
+    }, [searchTerm]);
 
 
     return (
